b5db5dab6997feaacfee8a3980cde22bfa1a0521
Fanil Nazirov <102851657+FanatiooN@users.noreply.github.com>
2024-04-04 18:03:53 +0300

    add config.py

---
+++ config.py
@@ -0,0 +1,38 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    API_ID: str
+    API_HASH: str
+
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+
+    CLASS_TYPES = {
+        3: "Русский язык",
+        40: "Алгебра",
+        1: "Математика",
+        9: "Базовая математика",
+        2: "Обществознание",
+        7: "История",
+        6: "Литература",
+        10: "Английский язык",
+        12: "Химия",
+        4: "Биология",
+        5: "Физика",
+        11: "Информатика",
+        25: "География",
+    }
+
+    @property
+    def database_url(self):
+        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?client_encoding=utf8'"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
