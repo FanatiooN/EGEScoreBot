302f4b2a16a833ad2d5b19f40471bf3da410976a
Fanil Nazirov <102851657+FanatiooN@users.noreply.github.com>
2024-04-05 15:25:22 +0300

    add functions for insert/select/delete info

--- sql/core.py
+++ sql/core.py
@@ -1,20 +1,18 @@
 from sql.database import engine
 from sql.models import metadata, students, subjects, student_subject
-from sqlalchemy import insert
+from sqlalchemy import insert, select, delete
 
 
 def create_tables():
-    engine.echo = False
     metadata.drop_all(engine, checkfirst=True)
     metadata.create_all(engine)
-    engine.echo = True
 
 
-def insert_student(name: str):
+def insert_student(telegram_id: int, name: str):
     with engine.connect() as conn:
         statement = insert(students).values(
             [
-                {"student_name": name},
+                {"telegram_id": telegram_id, "student_name": name},
             ]
         )
         conn.execute(statement)
@@ -31,3 +29,60 @@ def insert_subject(subject_id: int, subject_name: str):
         conn.execute(statement)
         conn.commit()
 
+
+def insert_scores(student_id: int, scores: list[tuple[int, int]]):
+    with engine.connect() as conn:
+        statement = insert(student_subject).values(
+            [
+                {
+                    "student_id": student_id,
+                    "subject_id": subject_id,
+                    "score": score,
+                }
+                for subject_id, score in scores
+            ]
+        )
+        conn.execute(statement)
+        conn.commit()
+
+
+def get_student_id(telegram_id: int):
+    with engine.connect() as conn:
+        query = select(students.c.student_id).where(
+            students.c.telegram_id == telegram_id
+        )
+
+        result = conn.execute(query)
+
+        student_id = result.scalar()
+
+        return student_id
+
+
+def get_scores(student_id: int):
+    with engine.connect() as conn:
+        query = select(student_subject).where(
+            student_subject.c.student_id == student_id
+        )
+
+        result = conn.execute(query)
+
+        scores = result.fetchall()
+
+        return scores
+
+
+def delete_student(student_id: int):
+    with engine.connect() as conn:
+        query = delete(students).where(students.c.student_id == student_id)
+
+        conn.execute(query)
+
+
+def delete_scores(student_id: int):
+    with engine.connect() as conn:
+        query = delete(student_subject).where(
+            student_subject.c.student_id == student_id
+        )
+
+        conn.execute(query)
